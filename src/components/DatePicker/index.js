dde7fc279b3369209ca334732a00f65761adbd7e
Guadalupe Torres <guadalupe@uulala.io>
2021-10-22 20:34:02 -0500

    datepicker image calendar

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { verticalScale } from 'react-native-size-matters';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { View, Text, Divider,ButtonRounded } from '@components';
+import React, { useState } from 'react';
+import { scale, verticalScale } from 'react-native-size-matters';
+import { View, Text, ImageResize } from '@components';
 import InputError from '@components/FloatingLabelInput/InputError';
 import PropTypes from 'prop-types';
-import { Button, Modal,TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import ModalDate from './modalDate';
 import { useSelector} from 'react-redux';
 import { formatDate } from '@utils/formatters';
-import Colors from '@styles/Colors';
+import blueCalendar from '@assets/icons/blue-calendar.png';
 import Styles from './styles';
+
+
 const DatePicker = ({ error, label, value, onSelect, languages, onFill, ...props }) => {
   const redux = useSelector(state => state);
   const appData = redux.user;
@@ -26,8 +27,21 @@ const DatePicker = ({ error, label, value, onSelect, languages, onFill, ...props
   return (
     <View>
       <TouchableOpacity  style={Styles.containerBirth}  onPress={() => setShowModalDates(!showModalDates)}>
-        <Text blue02 h10 medium>{label}</Text>
-        <Text white h13 medium>{valueData?formatDate(valueData):'MM/DD/YYYY'}</Text>
+        <View row>
+          <View flex-1 column>
+            <Text blue02 h10 medium>{label}</Text>
+            <Text white h13 medium>{valueData?formatDate(valueData):'MM/DD/YYYY'}</Text>
+          </View>
+          <View right centerH centerV>
+            <ImageResize
+              source={blueCalendar}
+              height={verticalScale(21)}
+              width={scale(21)}
+            />
+          </View>
+          
+        </View>
+        
       </TouchableOpacity>
       <InputError error={error} />
       <ModalDate visible={showModalDates} 
